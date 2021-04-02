4f93a1fdcf0889c92da33306cabfb928075f315b
zhangshenqi <shenqiz1995@gmail.com>
2021-04-02 00:33:39 -0400

    Add UDP methods in the connection manager.

--- ConnectionManager.java
+++ ConnectionManager.java
@@ -1,8 +1,14 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.RandomAccessFile;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.text.SimpleDateFormat;
@@ -10,16 +16,57 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A connection manager which provides network methods for nodes in distributed systems.
+ * @author Shenqi Zhang
+ *
+ */
 public abstract class ConnectionManager {
-    
+    /**
+     * Operations provided in this connection manager.
+     */
     private enum OPERATION {SEND_REQUEST, RECEIVE_REQUEST,
                             SEND_RESPONSE, RECEIVE_RESPONSE,
                             SEND_MESSAGE, RECEIVE_MESSAGE};
-    
+    /**
+     * Size of the packet data when receiving message.
+     */
+    private static final int BUF_SIZE = 100;
+    /**
+     * Name of this node in the distributed system.
+     */
     private String name;
+    /**
+     * Peer nodes in the distributed system.
+     */
     private Map<String, Peer> peers;
+    /**
+     * The socket to send and receive UDP messages.
+     */
+    private DatagramSocket datagramSocket;
+    /**
+     * The log writer.
+     */
+    private PrintStream logWriter;
     
-    public ConnectionManager(String name, boolean launchTCPServer) {
+    /**
+     * Constructs a connection manager.
+     * @param name the name of this node in the distributed system
+     * @param launchTCPServer true if a TCP server will be launched
+     * @param launchUDPServer true if a UDP server will be launched
+     */
+    public ConnectionManager(String name, boolean launchTCPServer, boolean launchUDPServer) {
+        this(name, launchTCPServer, launchUDPServer, null);
+    }
+    
+    /**
+     * Constructs a connection manager.
+     * @param name the name of this node in the distributed system
+     * @param launchTCPServer true if a TCP server will be launched
+     * @param launchUDPServer true if a UDP server will be launched
+     * @param logName the name of the log file; if null, log will be written in stdout
+     */
+    public ConnectionManager(String name, boolean launchTCPServer, boolean launchUDPServer, String logName) {
         this.name = name;
         peers = new HashMap<String, Peer>();
         RandomAccessFile file = null;
@@ -31,6 +78,7 @@ public abstract class ConnectionManager {
                 Peer peer = new Peer(strs[0], strs[1], Integer.valueOf(strs[2]));
                 peers.put(strs[0], peer);
             }
+            datagramSocket = new DatagramSocket(peers.get(name).backendPort);
         }  catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -44,41 +92,28 @@ public abstract class ConnectionManager {
         if (launchTCPServer) {
             new Thread(new TCPServer(peers.get(name).backendPort)).start();
         }
-    }
-    
-    protected abstract void handleRequest(String source, String request);
-    
-    private void printLog(OPERATION operation, String host, String text) {
-        StringBuilder sb = new StringBuilder(new SimpleDateFormat("HH:mm:ss.SSS").format(new Date()));
-        sb.append(' ').append(name);
-        switch (operation) {
-        case SEND_REQUEST:
-            sb.append(" sends request to ");
-            break;
-        case RECEIVE_REQUEST:
-            sb.append(" receives request from ");
-            break;
-        case SEND_RESPONSE:
-            sb.append(" sends response to ");
-            break;
-        case RECEIVE_RESPONSE:
-            sb.append(" receives response from ");
-            break;
-        case SEND_MESSAGE:
-            sb.append(" sends message to ");
-            break;
-        case RECEIVE_MESSAGE:
-            sb.append(" receives message from ");
-            break;
+        
+        if (launchUDPServer) {
+            new Thread(new UDPServer()).start();
         }
-        sb.append(host).append(": ").append(text);
-        System.out.println(sb.toString());
+        
+        if (logName == null) {
+            logWriter = System.out;
+        } else {
+            try {
+                logWriter = new PrintStream(new FileOutputStream(logName));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
     
-    private void printLog(String text) {
-        System.out.println(text);
-    }
-    
+    /**
+     * Sends request to the specified destination in the distributed system.
+     * @param destination the specified destination in the distributed system
+     * @param request request
+     * @return response from the destination, or null if fails
+     */
     protected String sendRequest(String destination, String request) {
         printLog(OPERATION.SEND_REQUEST, destination, request);
         
@@ -92,6 +127,13 @@ public abstract class ConnectionManager {
         return sendRequest(peer, request, peer.clientSocket == null);
     }
     
+    /**
+     * Sends request to the specified peer node in the distributed system.
+     * @param peer the specified peer node
+     * @param request request
+     * @param rebuildConnection true if a new TCP connection will be built
+     * @return response from the destination, or null if fails
+     */
     private String sendRequest(Peer peer, String request, boolean rebuildConnection) {
         if (rebuildConnection) {
             try {
@@ -127,6 +169,18 @@ public abstract class ConnectionManager {
         return response;
     }
     
+    /**
+     * Handles the request from the source.
+     * @param source source of the request in the distributed system
+     * @param request request
+     */
+    protected abstract void handleRequest(String source, String request);
+    
+    /**
+     * Sends response to the source in the distributed system.
+     * @param source source in the distributed system
+     * @param response response
+     */
     protected void sendResponse(String source, String response) {
         printLog(OPERATION.SEND_RESPONSE, source, response);
         
@@ -142,13 +196,146 @@ public abstract class ConnectionManager {
         peer.serverWriter.println(response);
     }
     
+    /**
+     * Sends message to the specified destination in the distributed system.
+     * @param destination the specified destination in the distributed system
+     * @param message message
+     */
+    protected void sendMessage(String destination, String message) {
+        printLog(OPERATION.SEND_MESSAGE, destination, message);
+        
+        if (!peers.containsKey(destination)) {
+            printLog("Error: No such destination!");
+            return;
+        }
+        
+        message = (new StringBuilder(name)).append(',').append(message).toString();
+        Peer peer = peers.get(destination);
+        byte[] buf = message.getBytes();
+        try {
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, InetAddress.getByName(peer.address), peer.backendPort);
+            datagramSocket.send(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Handles the message from the source
+     * @param source the source in the distributed system
+     * @param message message
+     */
+    protected abstract void handleMessage(String source, String message);
+    
+    /**
+     * Prints the log.
+     * @param operation operation
+     * @param host host
+     * @param text text
+     */
+    private void printLog(OPERATION operation, String host, String text) {
+        StringBuilder sb = new StringBuilder(new SimpleDateFormat("HH:mm:ss.SSS").format(new Date()));
+        sb.append(' ').append(name);
+        switch (operation) {
+        case SEND_REQUEST:
+            sb.append(" sends request to ");
+            break;
+        case RECEIVE_REQUEST:
+            sb.append(" receives request from ");
+            break;
+        case SEND_RESPONSE:
+            sb.append(" sends response to ");
+            break;
+        case RECEIVE_RESPONSE:
+            sb.append(" receives response from ");
+            break;
+        case SEND_MESSAGE:
+            sb.append(" sends message to ");
+            break;
+        case RECEIVE_MESSAGE:
+            sb.append(" receives message from ");
+            break;
+        }
+        sb.append(host).append(": ").append(text);
+        logWriter.println(sb.toString());
+    }
+    
+    /**
+     * Prints the log.
+     * @param text
+     */
+    private void printLog(String text) {
+        logWriter.println(text);
+    }
+    
+    /**
+     * Peer node in the distributed system.
+     *
+     */
+    private class Peer {
+        /**
+         * Name of the peer.
+         */
+        private String name;
+        /**
+         * IP address of the peer.
+         */
+        private String address;
+        /**
+         * Backend port of the peer.
+         */
+        private int backendPort;
+        /**
+         * Socket which sends data to TCP server of the peer.
+         */
+        private Socket clientSocket;
+        /**
+         * Reader which reads data from the TCP server of the peer.
+         */
+        private BufferedReader clientReader;
+        /**
+         * Writer which writes data to the TCP server of the peer. 
+         */
+        private PrintWriter clientWriter;
+        /**
+         * Writer which writes data to the TCP client of the peer.
+         */
+        private PrintWriter serverWriter;
+        
+        /**
+         * Constructs a peer node with specified name, address and backend port.
+         * @param name name
+         * @param address address
+         * @param backendPort backend port
+         */
+        Peer(String name, String address, int backendPort) {
+            this.name  = name;
+            this.address = address;
+            this.backendPort = backendPort;
+        }
+    }
+    
+    /**
+     * TCP server of this node.
+     *
+     */
     private class TCPServer implements Runnable {
+        /**
+         * Backend port of this node.
+         */
         private int backendPort;
         
+        /**
+         * Constructs a TCP server with specified backend port.
+         * @param backendPort
+         */
         TCPServer(int backendPort) {
             this.backendPort = backendPort;
         }
         
+        /**
+         * Keep listening. When a socket is accpeted, a TCP client handler socket is launched.
+         */
         @Override
         public void run() {
             ServerSocket serverSocket = null;
@@ -171,13 +358,27 @@ public abstract class ConnectionManager {
         }
     }
     
+    /**
+     * TCP client handler.
+     *
+     */
     private class TCPClientHandler implements Runnable {
+        /**
+         * Socket of this connection.
+         */
         private Socket socket;
         
+        /**
+         * Constructs a TCP client handler with specified socket.
+         * @param socket
+         */
         TCPClientHandler(Socket socket) {
             this.socket = socket;
         }
         
+        /**
+         * Receives requests from the client and handles the requests.
+         */
         @Override
         public void run() {
             try {
@@ -202,20 +403,32 @@ public abstract class ConnectionManager {
         }
     }
     
-    private class Peer {
-        private String name;
-        private String address;
-        private int backendPort;
-        private Socket clientSocket;
-        private BufferedReader clientReader;
-        private PrintWriter clientWriter;
-        private PrintWriter serverWriter;
-        
-        Peer(String name, String address, int backendPort) {
-            this.name  = name;
-            this.address = address;
-            this.backendPort = backendPort;
+    /**
+     * UDP server of this node.
+     *
+     */
+    private class UDPServer implements Runnable {
+        /**
+         * Receives and handles messages.
+         */
+        @Override
+        public void run() {
+            try {
+                byte[] buf = new byte[BUF_SIZE];
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                while (true) {
+                    datagramSocket.receive(packet);
+                    buf = packet.getData();
+                    String message = new String(buf, 0, packet.getLength());
+                    int index = message.indexOf(',');
+                    String source = message.substring(0, index);
+                    message = message.substring(index + 1);
+                    printLog(OPERATION.RECEIVE_MESSAGE, source, message);
+                    handleMessage(source, message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
-    
 }
