ef3d0df10b9e88f754e678fb191dcb4ed74ef71c
zhangshenqi <shenqiz1995@gmail.com>
2021-04-01 21:37:17 -0400

    Add a sample node to test connection manager.

---
+++ SampleNode.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class SampleNode extends ConnectionManager {
+    public SampleNode(String name) {
+        super(name, true);
+    }
+    
+    @Override
+    protected void handleRequest(String source, String request) {
+        sendResponse(source, request);
+    }
+    
+    /**
+     * A shell which sends user-specified requests and receives responses.
+     * @param args arguments
+     */
+    public static void main(String[] args) {
+        SampleNode node = new SampleNode(args[0]);
+        
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println();
+            System.out.println("1: send a request");
+            System.out.println("2: send a message");
+            System.out.println("X: kill");
+            System.out.println("Please input your operation:");
+            String operation = scanner.next();
+            
+            if (operation.toLowerCase().equals("x")) {
+                scanner.close();
+                System.exit(0);
+            }
+            
+            if (!operation.matches("[1-2]")) {
+                System.out.println("Error: Invalid operation!");
+                continue;
+            }
+            
+            System.out.println("Please input the destination:");
+            String destination = scanner.next();
+            
+            if (operation.equals("1")) {
+                System.out.println("Please input your request:");
+                String request = scanner.next();
+                node.sendRequest(destination, request);
+            } else {
+                System.out.println("Please input your message:");
+//                String message = scanner.next();
+//                node.sendMessage(destination, message);
+            }
+        }
+    }
+}
