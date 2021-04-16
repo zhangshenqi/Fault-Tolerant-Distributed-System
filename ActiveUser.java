6ccbe472442c6140cbb2cd17b40588d5231254bc
zhangshenqi <shenqiz1995@gmail.com>
2021-04-15 22:46:48 -0400

    In ActiveUser, directly call sendRequestToGroup.

--- ActiveUser.java
+++ ActiveUser.java
@@ -1,6 +1,6 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class ActiveUser extends User {
@@ -19,54 +19,23 @@ public class ActiveUser extends User {
             return "Error: Server is not available!";
         }
         
-        Set<String> responses = new HashSet<String>();
-        String[] members = membership.split(",");
-        List<Thread> threads = new ArrayList<Thread>(members.length);
-        for (String member : members) {
-            Thread thread = new Thread(new RequestSender(responses, member, request));
-            threads.add(thread);
-            thread.start();
-        }
-        
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        Map<String, String> responses = sendRequestToGroup(Arrays.asList(membership.split(",")), request);
+        Set<String> set = new HashSet<String>();
+        for (String response : responses.values()) {
+            if (responses != null) {
+                set.add(response);
             }
         }
         
-        if (responses.size() > 1) {
+        if (set.size() > 1) {
             System.out.println("Error: Responses are different!");
             System.exit(0);
         }
         
-        if (responses.size() == 0) {
+        if (set.size() == 0) {
             return "Error: Server is not available!";
         } else {
-            return responses.iterator().next();
-        }
-    }
-    
-    private class RequestSender implements Runnable {
-        Set<String> responses;
-        String destination;
-        String request;
-        
-        RequestSender(Set<String> responses, String destination, String request) {
-            this.responses = responses;
-            this.destination = destination;
-            this.request = request;
-        }
-        
-        @Override
-        public void run() {
-            String response = sendRequest(destination, request);
-            if (response != null) {
-                synchronized(responses) {
-                    responses.add(response);
-                }
-            }
+            return set.iterator().next();
         }
     }
     
